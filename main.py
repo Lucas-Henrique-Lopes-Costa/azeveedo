620b177514b58b86d7b2b80c62f8cafed8538a70
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-06-15 22:25:06 -0300

    edit

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-# leia um arqquivo csv e teste cada link que está na coluna Imagens e coluna Nome e se não tiver status 200, coloque na coluna encontrado como false, alem disso vai criando um arquivo falta.md que mostra o Nome e o link que não foi encontrado depois da um quebra de linha
+# leia um arqquivo csv e teste cada link que está na coluna Imagens e se não tiver status 200, coloque na coluna encontrado como false
 
-import pandas as pd
+import pandas as pd 
 import requests
 
-df = pd.read_csv('teste.csv')
-f = open("falta.md", "w")
+df = pd.read_csv('arquivo.csv')
 
-for index, row in df.iterrows():
-    if requests.get(row['Imagens']).status_code != 200:
-        df.loc[index, 'encontrado'] = False
-        f.write(row['Nome'] + " " + row['Imagens'] + "\n")
-    if requests.get(row['Nome']).status_code != 200:
-        df.loc[index, 'encontrado'] = False
-        f.write(row['Nome'] + " " + row['Imagens'] + "\n")
+def testar_link(link):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            print('Link encontrado ' + link)
+            return True
+        else:
+            print('Link não encontrado ' + link)
+            # coloca no falta.md
+            with open('falta.md', 'a') as f:
+                f.write(
+                    f'Link não encontrado: {link}\n\n'
+                )
+            return False
+    except:
+        return False
+    
+df['encontrado'] = df['Imagens'].apply(testar_link)
 
-df.to_csv('teste.csv', index=False)
-f.close()
+df.to_csv('arquivo.csv', index=False)
+
+print('Arquivo salvo com sucesso')
